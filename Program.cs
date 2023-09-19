2343675365ce878bb126b301eea1c38b582d2d5c
carolinmimmie <carolinmimmie@gmail.com>
2023-09-19 12:38:21 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;//Slipper skriva console
+
+// En namnrymd används för att organisera och separera olika delar av din kod
+//All kod inom den här filen är nu en del av namnrymden "IMDb."
+namespace IMDb;
+
+//Detta är deklarationen av en klass med namnet "Program." Klassen fungerar som ingångspunkten 
+//för din applikation. Det är här programmet börjar köra när det startas. Det är vanligt att ha 
+//en klass med namnet "Program" eller liknande som innehåller Main-metoden.
+class Program
+{
+    //Detta är definitionen av Main-metoden inom klassen "Program." 
+    //Main-metoden är den punkt där programmet börjar utföra sin logik
+    //Den är alltid statisk (static) och har ingen returtyp (void).
+    //Det innebär att den inte returnerar något värde när den är klar.
+    public static void Main()
+    {
+
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,15 @@
+Lägg till README.md:
+# IMDb
+
+Enkel konsolapplikation för att söka information om filmer.
+
+## Kom igång
+
+1. Klona repot.
+
+2. Sätt upp databasen.
+
+`dotnet ef database update`
+
+3. Starta applikationen.
+
